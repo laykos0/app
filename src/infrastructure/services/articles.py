980f6704973771aac9b50f56dcca9b0d01547220
laykos0 <laykos0@protonmail.com>
2023-07-18 19:46:16 +0200

    [articles.py] Check against enum Role value instead of str

--- src/infrastructure/services/articles.py
+++ src/infrastructure/services/articles.py
@@ -3,7 +3,10 @@ from src.domain.articles import (
     ArticleId,
     ArticleUpdateDTO
 )
-from src.domain.users import UserInDB
+from src.domain.users import (
+    UserInDB,
+    Role
+)
 from src.infrastructure.exceptions import (
     ArticleNotFoundException,
     VersionNotFoundException,
@@ -21,15 +24,15 @@ from src.infrastructure.repositories.articles import (
 
 
 async def post(user: UserInDB, article_create_dto: ArticleCreateDTO):
-    if user.role == "admin":
+    if user.role == Role.admin:
         article = article_create_dto.to_document()
         article.version.new(user.id)
         return await insert(article)
-    raise InsufficientPermissionException(user.id, user.role)
+    raise InsufficientPermissionException(user.id, Role.admin)
 
 
 async def get(user: UserInDB, article_id: ArticleId):
-    if user.role == "admin":
+    if user.role == Role.admin:
         article = await find_from_all(article_id)
     else:
         article = await find(article_id)
@@ -39,7 +42,7 @@ async def get(user: UserInDB, article_id: ArticleId):
 
 
 async def get_versions(user: UserInDB, article_id: ArticleId):
-    if user.role == "admin":
+    if user.role == Role.admin:
         versions = await find_versions_all(article_id)
     else:
         versions = await find_versions(article_id)
@@ -49,7 +52,7 @@ async def get_versions(user: UserInDB, article_id: ArticleId):
 
 
 async def get_version(user: UserInDB, article_id: ArticleId, version_number: int):
-    if user.role == "admin":
+    if user.role == Role.admin:
         version = await find_version_all(article_id, version_number)
     else:
         version = await find_version(article_id, version_number)
@@ -58,27 +61,27 @@ async def get_version(user: UserInDB, article_id: ArticleId, version_number: int
     raise VersionNotFoundException(article_id, version_number)
 
 
-async def put(user: UserInDB, article_id: ArticleId,  article_update_dto: ArticleUpdateDTO):
-    if user.role == "admin":
+async def put(user: UserInDB, article_id: ArticleId, article_update_dto: ArticleUpdateDTO):
+    if user.role == Role.admin:
         article = await get(user, article_id)
         article_update = article_update_dto.to_document()
         article.patch(user.id, name=article_update.name, description=article_update.description,
                       price=article_update.price)
         return await insert(article)
-    raise InsufficientPermissionException(user.id, user.role)
+    raise InsufficientPermissionException(user.id, Role.admin)
 
 
 async def confirm(user: UserInDB, article_id: ArticleId, approved: bool = True):
-    if user.role == "admin":
+    if user.role == Role.admin:
         article = await get(user, article_id)
         article.patch(user.id, approved=approved)
         return await insert(article)
-    raise InsufficientPermissionException(user.id, user.role)
+    raise InsufficientPermissionException(user.id, Role.admin)
 
 
 async def remove(user: UserInDB, article_id: ArticleId, deleted: bool = True):
-    if user.role == "admin":
+    if user.role == Role.admin:
         article = await get(user, article_id)
         article.patch(user.id, deleted=deleted)
         return await insert(article)
-    raise InsufficientPermissionException(user.id, user.role)
+    raise InsufficientPermissionException(user.id, Role.admin)
